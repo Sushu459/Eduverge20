9caf7c31c2cc1d049c9ae110e26a2951b7eae604
T Sushmanth <tsushmanth@gmail.com>
2025-12-18 00:07:26 +0530

    updated all the dependencies

--- vite-config.js
+++ vite-config.js
@@ -1,11 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-    open: true
+  build: {
+    outDir: 'dist',
+    sourcemap: false, // Disable in production
+    minify: 'terser',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vendor': ['react', 'react-dom', '@supabase/supabase-js']
+        }
+      }
+    }
   }
 })
